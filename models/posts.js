2c87be29eead7d1100dbe26da376872d78d251a8
Sachin Kumar <mail.sachin161@gmail.com>
2023-03-06 19:01:18 +0530

    makepost schema and api added

--- models/posts.js
+++ models/posts.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const post = mongoose.Schema(
   {
-    username: { type: String, require: true, unique: true },
-    email: { type: String, require: true, unique: true },
+    username: { type: String, require: true },
+    email: { type: String, require: true },
     title: { type: String, require: true, unique: true },
     body: { type: String, require: true },
     tag: { type: String, require: true },
